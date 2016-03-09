99e448c402586bbdccb871e86379b43af44cdc02
shivgupt <shivgupt@indiana.edu>
2016-03-08 22:26:05 -0500

    error handleling

--- system/shmat.c
+++ system/shmat.c
@@ -1,7 +1,14 @@
 #include <xinu.h>
 char *shmat(int16 shmid,char *shmaddr, int16 shmflag){
+	char *temp;
 	if(shmtab[shmid].key == -1)
 		return NULL;
-	if(shmaddr !=NULL) shmaddr =  shmtab[shmid].shmemory;
-	return shmtab[shmid].shmemory;
+	if(shmaddr !=NULL)
+	{
+	 	shmaddr =  shmtab[shmid].shmemory;
+		return shmaddr;
+	}
+	temp = shmtab[shmid].shmemory;
+	return temp;
+	
 }
--- system/shmget.c
+++ system/shmget.c
@@ -1,11 +1,11 @@
 #include <xinu.h>
-uint16 shmget(int16 key, uint32 size, int16 flag){
+int16 shmget(int16 key, uint32 size, int16 flag){
 	int i;
 	struct shment *shmptr;
 	for (i = 0; i < NSHMENT; i++) {
 		if(shmtab[i].key == key){
 			if((flag) == SHM_CREAT){
-				return -1;		
+				return SYSERR;		
 			}else{
 				return i;
 			}
@@ -16,16 +16,16 @@ uint16 shmget(int16 key, uint32 size, int16 flag){
 		break;
 	}
 	if((flag & SHM_FETCH) != SHM_FETCH){
-		return -1;	
+		return SYSERR;	
 	}	
 	shmptr = &shmtab[i];
 	shmptr->key = key;
 	shmptr->refcount = 0;
 	shmptr->size = size;
 	if((shmptr->shmemory = getmem(sizeof(char)*size))==SYSERR){
-		return -1;
+		return SYSERR;
 	}else{
 		return i;
 	}
-	return -1;
+	return SYSERR;
 }
