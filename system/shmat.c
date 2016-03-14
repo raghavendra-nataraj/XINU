1ee24d72a0c887c34d0b8df4e96e8ee879ddb99c
shivgupt <shivgupt@indiana.edu>
2016-03-14 01:32:29 -0400

    flag handling

--- system/shmat.c
+++ system/shmat.c
@@ -3,11 +3,18 @@ char *shmat(int16 shmid,char *shmaddr, int16 shmflag){
 	char *temp;
 	if(shmtab[shmid].key == -1)
 		return NULL;
-	if(shmaddr !=NULL)
-	{
-	 	shmaddr =  shmtab[shmid].shmemory;
-		return shmaddr;
+	if(shmflag == SHM_RND){
+
+		if(shmaddr !=NULL){
+	 		shmaddr =  shmtab[shmid].shmemory;
+			return shmaddr;
+		}
+		else{
+			temp = shmtab[shmid].shmemory;
+			return temp;	
+		}
 	}
+	/* shmflag = SHM_RDONLY */
 	temp = shmtab[shmid].shmemory;
 	return temp;
 	
