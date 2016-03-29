ae4f906b0c7d4cf7c92b87b98470779cef56dacc
shivgupt <shivgupt@indiana.edu>
2016-03-28 22:38:09 -0400

    future_set

--- system/future_set.c
+++ system/future_set.c
@@ -1,6 +1,16 @@
 #include "xinu.h"
 #include "future.h"
 syscall future_set(future_t* f, int value){
-
-	
+	if(f.state == FUTURE_VALID)
+	{
+		pid32 pid = getpid();
+		enqueue(f->set_queue,pid);
+	}
+	if(f.state == FUTURE_EMTPY || f.state == FUTURE_WAITING)
+	{
+		f.state = FUTURE_VALID;
+		pid32 pid = dequeue(f->get_queue);
+		resume(pid);
+	}
+	return 	OK;
 }
