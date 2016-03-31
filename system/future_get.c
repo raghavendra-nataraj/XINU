8056c3b0cb7a0dfc1084ea0c83fb0d872f72ce3b
ragha <ragha@ragha.com>
2016-03-30 22:58:58 -0400

    All working

--- system/future_get.c
+++ system/future_get.c
@@ -5,11 +5,22 @@ syscall future_get(future_t* f, int* value){
 	if(f->state == FUTURE_EMPTY || f->state == FUTURE_WAITING){
 		pid32 pid = getpid();
 		f->state = FUTURE_WAITING;
+		//printf("get enque = %d\n",pid);
 		pid = enqueue(pid,f->get_queue);
 		suspend(pid);
 	}
 	
 	if(f->state == FUTURE_VALID){
+		pid32 pid;
+		if(f->mode == FUTURE_QUEUE){
+			if((pid = dequeue(f->set_queue))!=EMPTY){
+				//printf("get deque = %d\n",pid);
+				f->state = FUTURE_WAITING;
+				resume(pid);
+			}else{
+				f->state = FUTURE_WAITING;
+			}
+		}
 		if(isempty(f->get_queue))
 			f->state = FUTURE_EMPTY;
 		*value = f->value;
--- system/future_set.c
+++ system/future_set.c
@@ -9,17 +9,33 @@ syscall future_set(future_t* f, int value){
 			return SYSERR;
 		}else{
 			pid32 pid = getpid();
+			//printf("set enque = %d\n",pid);
 			enqueue(pid,f->set_queue);
+			suspend(pid);
 		}
 	}
 	if(f->state == FUTURE_EMPTY || f->state == FUTURE_WAITING)
 	{
+		if(isempty(f->get_queue)){
+			pid32 pid = getpid();
+			//printf("set self enque = %d\n",pid);
+			enqueue(pid,f->set_queue);
+			suspend(pid);
+		}
 		f->state = FUTURE_VALID;
 		f->value = value;	
 		pid32 pid;
-		while((pid = dequeue(f->get_queue))!=EMPTY){
-			resume(pid);
+		if(f->mode == FUTURE_QUEUE){
+			if((pid = dequeue(f->get_queue))!=EMPTY){
+				//printf("get deque = %d\n",pid);
+				resume(pid);
+			}
+		}else{	
+			while((pid = dequeue(f->get_queue))!=EMPTY){
+				resume(pid);
+			}
 		}
+		
 	}
 	return 	OK;
 }
